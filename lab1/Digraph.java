5b0f543cca33013b0868592928af665f91fb2a9a
wonner <yw1997yw@126.com>
2017-10-20 10:30:46 +0800

    pmd review

---
+++ lab1/Digraph.java
@@ -0,0 +1,396 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ * @author jtt & gyx
+ */
+public class Digraph {
+  
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  final private transient Vnode[] vlist;
+
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private class Vnode {
+    /**
+     * 
+     * @author jtt & gyx
+     */
+    public transient String data;
+    /**
+     * 
+     * @author jtt & gyx
+     */
+    public transient Enode first;
+    /**
+     * 
+     * @author jtt & gyx
+     */
+    public transient Integer num;
+    /**
+    * 
+    * @author jtt & gyx
+    */
+    public transient int weight;
+    /**
+     * 
+     * @author jtt & gyx
+     */
+     public Vnode() {
+       weight = 1;
+     }
+  }
+  
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private class Enode {
+    /**
+     * 
+     * @author jtt & gyx
+     */
+    public transient String data;
+    /**
+     * 
+     * @author jtt & gyx
+     */
+    public transient Enode next;
+    /**
+     * 
+     * @author jtt & gyx
+     */
+    public transient boolean visit;
+    /**
+     * 
+     * @author jtt & gyx
+     */
+    public transient int weight;
+    /**
+    * 
+    * @author jtt & gyx
+    */
+    public Enode() {
+      weight = 0;
+      visit = false;
+    }
+    
+  }
+
+  /**  constructor
+   * 
+   * @param str.
+   */
+  public Digraph(final String... str) {
+    final ArrayList<String> set = new ArrayList<String>();
+    for (int i = 0;i < str.length;i++) {
+      if (!set.contains(str[i])) {
+        set.add(str[i]);
+      }
+    }
+    final Object[] schange = set.toArray();
+    vlist = new Vnode[set.size()];
+    for (int i = 0;i < set.size();i++) {
+      vlist[i] = new Vnode();
+      vlist[i].data = (String)schange[i];
+      vlist[i].first = null;
+      vlist[i].num = 0;
+    }
+    for (int k = 1;k < str.length;k++) {
+      final int loc = index(str[k - 1]);
+      Enode node1 = vlist[loc].first;
+      if (node1 == null) {
+        final Enode node = new Enode();
+        node.data = str[k];
+        node.visit = false;
+        node.weight = 1;
+        node.next = vlist[loc].first;
+        vlist[loc].first = node;
+        vlist[loc].num++;
+      } else {
+        while (node1 != null) {
+          if (node1.data.equals(str[k])) {
+            node1.weight++;
+            break;
+          }
+          node1 = node1.next;
+        }
+      }
+    }
+  }
+  
+  /** find the index of the words in vertex list.
+   * 
+   * @param  st.
+   * @return index of the word.
+   */
+  public final int index(final String str) {
+    int locOfVertexx = -1;
+    for (int i = 0;i < vlist.length;i++) {
+      if (vlist[i].data.equals(str)) {
+        locOfVertexx = i;
+      }
+      if (i == vlist.length - 1) {
+        break;
+      }
+    }
+    return locOfVertexx;
+  }
+
+  /** find bridge words between word1 and word2.
+   * 
+   * @param word1.
+   * @param word2.
+   * @return bridge words list.
+   */
+  public final String[] queryBridgeWords(final String word1,final String word2) {
+    final int word1Loc = index(word1);
+    final int word2Loc = index(word2);
+    final ArrayList<String> str = new ArrayList<String>();
+    if (word1Loc == -1 && word2Loc != -1) {
+      System.out.println("No " + word1 + " in the graph!");
+    } else if (word1Loc != -1 && word2Loc == -1) {
+      System.out.println("No " + word2 + " in the graph!");
+    } else if (word1Loc == -1 && word2Loc == -1) {
+      System.out.println("No " + word1 + " and " + word2 + " in the graph!");
+    } else {
+      Enode eNode = vlist[word1Loc].first;
+      while (eNode != null) {
+        Enode eNode2 = vlist[index(eNode.data)].first;
+        while (eNode2 != null) {
+          if (eNode2.data.equals(word2)) {
+            str.add(eNode.data);
+            break;
+          }
+          eNode2 = eNode2.next;
+        }
+        eNode = eNode.next;
+      }
+    }
+    if (str.isEmpty()) {
+      System.out.println("No bridge word from " + word1 + " to " + word2);
+    } else {
+      System.out.println("The bridge words from " + word1 + " to " + word2 + " is " + str);
+    }
+    String []newst = new String[str.size()];
+    final Object []schange = str.toArray();
+    for (int i = 0;i < str.size();i++) {
+      newst[i] = (String)schange[i];
+    }
+    return newst;
+  }
+
+  /** calculate random path.
+   * 
+   * @return random path list
+   */
+  public ArrayList<String> randWalk() {
+    final Random rand = new Random();
+    int loc1 = rand.nextInt(vlist.length);
+    final ArrayList<String> randomList = new ArrayList<String>();
+    randomList.add(vlist[loc1].data);
+    while (true) {
+      if (vlist[loc1].num == 0) {
+        break;
+      }
+      final int loc2 = rand.nextInt(vlist[loc1].num);
+      Enode randNode = vlist[loc1].first;
+      for (int k = 0;k < loc2;k++) {
+        randNode = randNode.next;
+      }
+      randomList.add(randNode.data);
+      if (randNode.visit) {
+        break;
+      }  
+      randNode.visit = true;
+      loc1 = index(randNode.data);
+    }
+    return randomList;
+  }
+
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public final String [] generateNewText(final String ...inputText) {
+    final ArrayList<String> newtext = new ArrayList<String>();
+    final Random rand = new Random();
+    for (int i = 0;i < inputText.length - 1;i++) {
+      newtext.add(inputText[i]);
+      final String[] bridgeLst = queryBridgeWords(inputText[i],inputText[i + 1]);
+      if (bridgeLst.length == 0) {
+        continue;
+      } else {
+        newtext.add(bridgeLst[rand.nextInt(bridgeLst.length)]);
+      }
+    }
+    newtext.add(inputText[inputText.length - 1]);
+    String []newtxtText = new String[newtext.size()];
+    final Object []sChange = newtext.toArray();
+
+    for (int i = 0;i < newtext.size();i++) {
+      newtxtText[i] = (String)sChange[i];
+    }
+    return newtxtText;
+  }
+  
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public final int [] dijkstra(final int sourceVertex, final int vertexNum,final int destVertex) {
+    int []stPath;
+    stPath = new int [vertexNum];
+    int []isVisit;
+    isVisit = new int [vertexNum];
+    final Queue<Vnode> queue = new PriorityQueue<>();
+    //初始化
+    final int Inf = Integer.MAX_VALUE;
+    for (int i  =  0; i < vertexNum; i++) {
+      vlist[i].weight  =  Inf;
+      stPath[i]  =  -1;     //每个顶点都无父亲节点
+      isVisit[i]  =  0;   //都未找到最短路
+    }
+    vlist[sourceVertex].weight  =  0;
+    queue.add(vlist[sourceVertex]);
+    while (!queue.isEmpty()) {
+      final Vnode vertexNode  =  queue.element();
+      queue.poll();
+      final int verLoc = index(vertexNode.data);
+      if (isVisit[verLoc] == 1) {
+        continue;
+      }
+      isVisit[verLoc]  =  1;
+      Enode edgeNode = vlist[verLoc].first;
+      while (edgeNode != null) {
+        final int tempv  =  index(edgeNode.data);
+        final int tempw  =  edgeNode.weight;
+        if (isVisit[tempv] == 0 && vlist[tempv].weight > (vlist[verLoc].weight + tempw)) {
+          vlist[tempv].weight  =  vlist[verLoc].weight + tempw;
+          stPath[tempv] = verLoc;
+          queue.add(vlist[tempv]);
+        }
+        edgeNode = edgeNode.next;
+      }
+    }
+    System.out.println("最短路径长为：" + vlist[destVertex].weight);
+    return stPath;
+  }
+  
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public final String[] calcShortestPath(final String word1, final String word2) {
+    final ArrayList<String> pList = new ArrayList<String>();
+    int [] stPath;
+    final int word1Loc = index(word1);
+    int word2Loc = index(word2);
+    int count = 0;
+    if (word1Loc == -1 || word2Loc == -1) {
+      System.out.println("单词不存在");
+    } else {
+      stPath = dijkstra(word1Loc, vlist.length,word2Loc);
+      while (word2Loc != -1) {
+        pList.add(vlist[word2Loc].data);
+        word2Loc = stPath[word2Loc];
+      }
+    }
+    String []stPath2 = new String[pList.size()];
+    final Object []sChange = pList.toArray();
+    for (count = 0;count < pList.size();count++) {
+      stPath2[count] = (String)sChange[count];
+    }
+    return stPath2;
+  }
+
+  /**
+   * 
+   * @author jtt & gyx
+   * @throws IOException 
+   */
+  public static String[] readfromfile(final String filepath) throws IOException {
+    String[] wordsList = {};
+    final FileReader fReader = new FileReader(filepath);
+    final BufferedReader bufReader = new BufferedReader(fReader);
+    final StringBuilder str0 = new StringBuilder();
+    String textLine ;
+    while ((textLine = bufReader.readLine())!= null) {
+      str0.append(textLine);
+      str0.append(String.valueOf(' '));
+    }
+    bufReader.close();
+    fReader.close();
+
+    final Scanner scan = new Scanner(str0.toString());
+    String str = scan.nextLine();
+    scan.close();
+    str = str.toLowerCase(Locale.ENGLISH);
+    str = str.replaceAll("[^a-z]", " ");
+    str = str.replaceAll("\\s{1,}", " ");
+    wordsList = str.split(" ");
+    return wordsList;
+  }
+
+  /** main function.
+   * 
+   * @param args.
+   * @throws IOException 
+   */
+  public static void main(final String[] args) throws IOException {
+    final String[] text = readfromfile("D:\\lab1\\java.txt");
+    if (text.length == 0) {
+      return;
+    }
+    final Digraph digrapg = new Digraph(text);
+    String aChoise = JOptionPane.showInputDialog("choose function(1,2,3,4,5,6):");
+    while (aChoise!=null &&!aChoise.equals("0")) {
+      switch (aChoise) {
+        case "3":
+          final String word1 = JOptionPane.showInputDialog("input word1:");
+          final String word2 = JOptionPane.showInputDialog("input word2:");
+          digrapg.queryBridgeWords(word1,word2);
+          break;
+        case "4":
+          final String[] newText = readfromfile("F:\\lab1\\newtext.txt");
+          for (int i = 0;i < newText.length;i++) {
+            System.out.print(newText[i] + " ");
+          }
+          break;
+        case "5":
+          final String wo1 = JOptionPane.showInputDialog("input word1:");
+          final String wo2 = JOptionPane.showInputDialog("input word2:");
+          final String [] shotestPath = digrapg.calcShortestPath(wo1,wo2);
+          for (int i = shotestPath.length - 1;i >= 0;i--) {
+            System.out.print(shotestPath[i] + " ");
+          }
+          break;
+        case "6":
+          final ArrayList<String> randList = digrapg.randWalk();
+          System.out.println(randList);
+          System.out.println("i changed the lab");
+          break;
+
+        default:
+          break;
+      }
+      aChoise = JOptionPane.showInputDialog("choose function(1,2,3,4,5,6):");
+    }
+  }
+
+}
--- lab1/GraphViz.java
+++ lab1/GraphViz.java
@@ -2,77 +2,153 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.logging.Logger;
 
+/**
+ * 
+ * @author jtt & gyx
+ */
+class GraphViz {
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private transient String runPath = "";
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private transient String dotPath = "";
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private transient String runOrder = "";
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private final static transient String DOTCODEFILE = "dotcode.txt";
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private final static transient String RESULTGIF = "dotGif";
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private final transient StringBuilder graph = new StringBuilder(20);
+  
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private final transient Runtime runtime = Runtime.getRuntime();
 
-class  GraphViz{
-    private String runPath = "";
-    private String dotPath = ""; 
-    private String runOrder="";
-    private String dotCodeFile="dotcode.txt";
-    private String resultGif="dotGif";
-    private StringBuilder graph = new StringBuilder();
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  private final static Logger LOG = Logger.getLogger("LAB1"); 
 
-    Runtime runtime=Runtime.getRuntime();
-
-    public void run() {
-        File file=new File(runPath);
-        file.mkdirs();
-        writeGraphToFile(graph.toString(), runPath);
-        creatOrder();
-        try {
-            runtime.exec(runOrder);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public void run() {
+    final File file = new File(runPath);
+    file.mkdirs();
+    writeGraphToFile(graph.toString(), runPath);
+    creatOrder();
+    try {
+      runtime.exec(runOrder);
+    } catch (IOException e) {
+      LOG.fine(e.toString());
     }
+  }
 
-    public void creatOrder(){
-        runOrder+=dotPath+" ";
-        runOrder+=runPath;
-        runOrder+="\\"+dotCodeFile+" ";
-        runOrder+="-T gif ";
-        runOrder+="-o ";
-        runOrder+=runPath;
-        runOrder+="\\"+resultGif+".gif";
-        System.out.println(runOrder);
+  /**
+   * 
+   * @author jtt & gyx
+   */
+
+  public void creatOrder() {
+    runOrder += dotPath + " ";
+    runOrder += runPath;
+    runOrder += "\\" + DOTCODEFILE + " ";
+    runOrder += "-T gif ";
+    runOrder += "-o ";
+    runOrder += runPath;
+    runOrder += "\\" + RESULTGIF + ".gif";
+  }
+
+  /**
+   * 
+   * @author jtt & gyx
+   */
+
+  public final void writeGraphToFile(final String dotcode, final String filename) {
+    try {
+      final File file = new File(filename + "\\" + DOTCODEFILE);
+      if (!file.exists()) {
+        file.createNewFile();
+      }
+      final FileOutputStream fos = new FileOutputStream(file);
+      fos.write(dotcode.getBytes());
+      fos.close();
+    } catch (IOException ioe) {
+      LOG.fine(ioe.toString());
     }
+  }
 
-    public void writeGraphToFile(String dotcode, String filename) {
-        try {
-            File file = new File(filename+"\\"+dotCodeFile);
-            if(!file.exists()){
-                file.createNewFile();
-            }
-            FileOutputStream fos = new FileOutputStream(file);
-            fos.write(dotcode.getBytes());
-            fos.close();
-        } catch (java.io.IOException ioe) { 
-            ioe.printStackTrace();
-        }
-     }  
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public GraphViz(final String runPath, final String dotPath) {
+    this.runPath = runPath;
+    this.dotPath = dotPath;
+  }
 
-    public GraphViz(String runPath,String dotPath) {
-        this.runPath=runPath;
-        this.dotPath=dotPath;
-    }
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public final void add(final String line) {
+    graph.append(String.valueOf('\t'));
+    graph.append(line);
+  }
 
-    public void add(String line) {
-        graph.append("\t"+line);
-    }
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public void addln(final String line) {
+    graph.append("\t" + line + "\n");
+  }
 
-    public void addln(String line) {
-        graph.append("\t"+line + "\n");
-    }
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public void addln() {
+    graph.append('\n');
+  }
 
-    public void addln() {
-        graph.append('\n');
-    }
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public void startGraph() {
+    graph.append("digraph G {\n");
+  }
 
-    public void start_graph() {
-        graph.append("digraph G {\n") ;
-    }
-
-    public void end_graph() {
-        graph.append("}") ;
-    }   
-} 
+  /**
+   * 
+   * @author jtt & gyx
+   */
+  public void endGraph() {
+    graph.append(String.valueOf('}'));
+  }
+}
